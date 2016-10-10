d47ef4e1f3ba384b14d8d4e155f26d8094892a01
DonalDoherty <dohertydonal98@gmail.com>
2016-10-10 01:47:35 +0100

    Starting Assignment 1.

---
+++ src/algorithms/Term.java
@@ -0,0 +1,15 @@
+package algorithms;
+
+public class Term {
+	
+	public int weight;
+	public String text;
+	
+	
+	public Term (int weight, String text)
+	{
+		this.weight = weight;
+		this.text = text;
+	}
+
+}
