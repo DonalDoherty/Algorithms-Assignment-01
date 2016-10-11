1c8a9e9e65566070865e362681c8ae64f3cd0ed6
DonalDoherty <dohertydonal98@gmail.com>
2016-10-11 01:04:48 +0100

    Introduced more Classes + Tests

---
+++ test/algorithms/BruteTest.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BruteTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/algorithms/QuickTest.java
@@ -0,0 +1,14 @@
+package algorithms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuickTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
