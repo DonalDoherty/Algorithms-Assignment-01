bde5c5365ce2989ac880d130a2908bbc6cd0aa18
Donal Doherty <dohertydonal98@gmail.com>
2016-10-22 23:00:01 +0100

    Created Driver Class Implemented weightOf function in BruteAutoComplete Began work on BruteTest

--- src/algorithms/BruteAutoComplete.java
+++ src/algorithms/BruteAutoComplete.java
@@ -1,19 +1,35 @@
 package algorithms;
 
 public class BruteAutoComplete implements AutoComplete {
-
+	TermList terms;
+   
+	// Returns the weight of the term, or 0.0 if no such term.
 	@Override
 	public double weightOf(String term) {
-		// TODO Auto-generated method stub
-		return 0;
+	if(term != null)
+	{
+		for(int i = 0; i<terms.termList.size(); i++)
+		{
+			if(term == terms.termList.get(i).getText())
+			{
+				return terms.termList.get(i).getWeight();
+			}
+		}
+	}
+		return 0.00;
 	}
 
+    // Returns the highest weighted matching term, or null if no matching term.
 	@Override
 	public String bestMatch(String prefix) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+    // Returns the highest weighted k matching terms (in descending order of weight), as an
+    // iterable.
+    // If fewer than k matches, return all matching terms (in descending order
+    // of weight).
 	@Override
 	public Iterable<String> matches(String prefix, int k) {
 		// TODO Auto-generated method stub
--- test/algorithms/BruteTest.java
+++ test/algorithms/BruteTest.java
@@ -2,13 +2,24 @@ package algorithms;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class BruteTest {
-
+	private TermList terms;
+	private BruteAutoComplete brute;
+	@Before
+	public void setUp() throws IOException
+	{
+		terms = new TermList();
+	}
+	
+	//Testing the weightOf method
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testWeightOf() {
+		assertEquals(5627187200.0, brute.weightOf("the"), 0.01);
 	}
 
 }
